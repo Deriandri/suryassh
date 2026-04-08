43dd6adc19321922eca170e001b118cfe1d9332e
Deriandri <deriandriananda82@gmail.com>
2026-04-08 01:43:10 -0700

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ZearGames - Premium Tunneling Solution</title>
+    <title>SuryaSSH - Premium WebSocket Tunneling Solution</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap" rel="stylesheet">
@@ -84,7 +84,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto text-center text-lg-start">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="ssh_list.php">SSH Server</a></li>
+                <li class="nav-item"><a class="nav-link" href="ssh_websocket.php">SSH Server</a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="subTrigger">V2Ray Service</a>
                     <ul class="dropdown-menu" id="subContent">
@@ -93,7 +93,7 @@
                         <li><a class="dropdown-item nav-link border-0" href="v2ray_trojan.php">Trojan Bypass</a></li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="status.php">Status</a></li>
+                <li class="nav-item"><a class="nav-link" href="server_status.php">Status</a></li>
             </ul>
         </div>
     </div>
